fe44019ddb7c15c353dd35aa5bba27353e42e9b2
moshfiqrony <moshfiqrony@gmail.com>
2019-10-13 16:40:06 +0600

    main app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+// Import React and Component
+import React, { Component } from 'react';
+// Import CSS from App.css
+import './App.css';
+// Import the Today component to be used below
+import Today from './Today/Today'
+// Import the History component to be used below
 
-function App() {
-  return (
-    <div>
-      <h1>
-        We are coming up with a new apps of pwa using the methods of service workers
-      </h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+          <section className="results--section">
+              <div className="container">
+                  <h1>PusherCoins is a realtime price information about<br></br> BTC, ETH and LTC.</h1>
+              </div>
+              <div className="results--section__inner">
+                  <Today />
+              </div>
+          </section>
+      </div>
+    );
+  }
 }
 
 export default App;
